70793fdbdb7dbadfaafc5a832d744d62ddda1ca9
shannon-nabors <shannon.nabors@gmail.com>
2019-02-23 20:06:34 -0500

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-// use this to copy code snippets or use your browser's console
+// function greet() {
+//     console.log(`my name is ${this.name}, hi!`)
+// }
+
+// function greet(customerOne, customerTwo) {
+//     console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`)
+// }
+
+// greet()
+//
+// let person = {
+//     name: 'Bob',
+//     greet: greet
+// }
+//
+// person.greet()
+//
+let sally = { name: 'Sally' }
+//
+// greet.call(sally)
+//
+// greet.apply(sally)
+//
+// greet.call(sally, 'Terry', 'George')
+//
+// greet.apply(sally, ['Terry', 'George'])
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`)
+}
+
+let newGreet = greet.bind(sally)
+
+newGreet('Bob')
+
+greet('Bob')
